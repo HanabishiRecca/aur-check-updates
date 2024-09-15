e2528256fa89f47470e9da95d621cec0658e1b11
Hanabishi <13597663+HanabishiRecca@users.noreply.github.com>
2024-09-15 00:20:55 +0000

    Generalize defaults

--- src/main.rs
+++ src/main.rs
@@ -14,12 +14,24 @@ use crate::{
 };
 use std::{env, error::Error, process::ExitCode};
 
+const DEFAULT_COLOR_MODE: ColorMode = ColorMode::Auto;
 const DEFAULT_DBPATH: &str = "/var/lib/pacman";
+const DEFAULT_IGNORES: &[&str] = &[];
+const DEFAULT_IGNORE_GROUPS: &[&str] = &[];
 const DEFAULT_IGNORE_SUFFIXES: &[&str] = &["-debug"];
 const DEFAULT_ENDPOINT: &str = "https://aur.archlinux.org/rpc/v5/info";
 const DEFAULT_TIMEOUT: u64 = 5000;
 
 type R = Result<(), Box<dyn Error>>;
+
+macro_rules! default {
+    ($option: expr, $default: expr $(,)?) => {
+        match $option {
+            Some(value) => value,
+            _ => $default,
+        }
+    };
+}
 
 pub fn check_updates(pkgs: Arr<(Str, Str)>, endpoint: &str, timeout: u64) -> R {
     if pkgs.is_empty() {
@@ -51,43 +63,41 @@ fn print_help() {
         include_str!("help.in"),
         PKG = env!("CARGO_PKG_NAME"),
         VER = env!("CARGO_PKG_VERSION"),
-        BIN_NAME = (|| bin.as_ref()?.file_name()?.to_str())().unwrap_or(env!("CARGO_BIN_NAME")),
+        BIN_NAME = default!(
+            (|| bin.as_ref()?.file_name()?.to_str())(),
+            env!("CARGO_BIN_NAME")
+        ),
     );
 }
 
 fn run() -> R {
-    let Some(config) = cli::read_args(std::env::args().skip(1))? else {
+    let Some(config) = cli::read_args(env::args().skip(1))? else {
         print_help();
         return Ok(());
     };
 
-    print::set_color_mode(config.color_mode().unwrap_or(ColorMode::Auto));
+    print::set_color_mode(default!(config.color_mode(), DEFAULT_COLOR_MODE));
     print::header("Checking AUR updates...");
 
-    let dbpath = config.dbpath().unwrap_or(DEFAULT_DBPATH);
-    let ignores = config.ignores().unwrap_or(&[]);
-    let ignore_groups = config.ignore_groups().unwrap_or(&[]);
-
-    let ignore_suffixes = match config.ignore_suffixes() {
-        Some(s) => s,
-        _ => &utils::copy(DEFAULT_IGNORE_SUFFIXES),
-    };
-
-    let repos = match config.repos() {
-        Some(r) => r,
-        _ => &io::find_repos(dbpath)?,
-    };
+    let dbpath = default!(config.dbpath(), DEFAULT_DBPATH);
+    let ignores = default!(config.ignores(), &utils::copy(DEFAULT_IGNORES));
+    let ignore_groups = default!(config.ignore_groups(), &utils::copy(DEFAULT_IGNORE_GROUPS));
+    let ignore_suffixes = default!(
+        config.ignore_suffixes(),
+        &utils::copy(DEFAULT_IGNORE_SUFFIXES)
+    );
+    let repos = default!(config.repos(), &io::find_repos(dbpath)?);
 
     let packages =
         alpm::find_foreign_packages(dbpath, repos, ignores, ignore_groups, ignore_suffixes)?;
 
-    let endpoint = config.endpoint().unwrap_or(DEFAULT_ENDPOINT);
-    let timeout = config.timeout().unwrap_or(DEFAULT_TIMEOUT);
+    let endpoint = default!(config.endpoint(), DEFAULT_ENDPOINT);
+    let timeout = default!(config.timeout(), DEFAULT_TIMEOUT);
     check_updates(packages, endpoint, timeout)
 }
 
 fn main() -> ExitCode {
-    print::set_color_mode(ColorMode::Auto);
+    print::set_color_mode(DEFAULT_COLOR_MODE);
     match run() {
         Err(e) => {
             print::error(e);
